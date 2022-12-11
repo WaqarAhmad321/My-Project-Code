c6793bbe1d84aff85c55a1937cb4f1bc378060b1
WaqarAhmad321 <106612382+WaqarAhmad321@users.noreply.github.com>
2022-12-11 22:00:31 +0500

    Add files via upload

---
+++ Python/Projects/calculator.py
@@ -0,0 +1,25 @@
+print("***********Welcome to Your Calculator***************")
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : "))
+# type casting (already studied in C language)
+add = n1 + n2
+print("Addition of", n1, "and", n2, "is", add)
+print("-----------Cool-----------")
+sub = n1 - n2
+print("Substraction of", n1, "and", n2, "is", sub)
+print("-----------Cool-----------")
+mul = n1 * n2
+print("Multiplication of", n1, "and", n2, "is", mul)
+print("-----------Cool-----------")
+div = n1 / n2
+print("Division of", n1, "and", n2, "is", div)
+print("-----------Cool-----------")
+mod = n1 % n2
+print("Modulus of", n1, "and", n2, "is", mod)
+print("-----------Cool-----------")
+expo = n1**n2
+print("Exponential of", n1, "w.r.t", n2, "is", expo)
+print("-----------Cool-----------")
+flo = n1 // n2
+print("Float of", n1, "and", n2, "is", flo)
+print("*********Thank You ! For using this calculator********")
