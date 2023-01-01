2fae25479e55f05bf075dafc6e1b49dc4c11806e
WaqarAhmad321 <106612382+WaqarAhmad321@users.noreply.github.com>
2023-01-01 21:16:40 +0500

    Add files via upload

---
+++ Python/Lists/copy_method.py
@@ -0,0 +1,5 @@
+# copy method is used to copy the content of a list into another variable
+l = ["Hello", "World", 2]
+c = l.copy()
+c[0] = 0
+print(c)
---
+++ Python/Mini Projects/quize_test.py
@@ -0,0 +1,48 @@
+print("***************Welcome to Quiz Compitetion***************")
+queslist = ["When Pakistan became an independent country (year) : ",
+            "When bangladesh separated from Pakistan : ", "How many provinces of Pakistan: ",
+            "In which year did Pakistan win the Cricket World Cup : ", "When did Pakistan win Olympic gold medal in Hockey for the first time"]
+anslist = ["1947", "1971", "4", "1992", "1960"]
+score = 0
+# q no 1
+print(f"Q no 1. {queslist[0]}")
+ans1 = input()
+if (ans1 == anslist[0]):
+    print("Your answer is Correct")
+    score = score + 1
+else:
+    print("Your answer is Incorrect")
+# q no 2
+print(f"Q no 2. {queslist[1]}")
+ans1 = input()
+if (ans1 == anslist[1]):
+    print("Your answer is Correct")
+    score = score + 1
+else:
+    print("Your answer is Incorrect")
+# q no 3
+print(f"Q no 3. {queslist[2]}")
+ans1 = input()
+if (ans1 == anslist[2]):
+    print("Your answer is Correct")
+    score = score + 1
+else:
+    print("Your InYour answer is Incorrect")
+# q no 4
+print(f"Q no 4. {queslist[3]}")
+ans1 = input()
+if (ans1 == anslist[3]):
+    print("Your answer is Correct")
+    score = score + 1
+else:
+    print("Your answer is Incorrect")
+ # q no 5
+print(f"Q no 5. {queslist[4]}")
+ans1 = input()
+if (ans1 == anslist[4]):
+    print("Your answer is Correct")
+    score = score + 1
+else:
+    print("Your answer is Incorrect")
+print("***************Thanks ! Your result is given below :-)***************")
+print(f"Your scored {score} out 5")
